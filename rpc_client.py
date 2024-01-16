8d4e3c429bd62832240ea6057f4c8384b6fca644
S ABHISHEK <79701277+ABHISHEKSUBHASHSWAMI@users.noreply.github.com>
2024-01-16 21:12:07 +0530

    Update rpc_client.py

--- rpc_client.py
+++ rpc_client.py
@@ -1,9 +1,9 @@
-
+ 
 
 #client side 
 
 from xmlrpc import client
 
-proxy=client.ServerProxy("http://localhost:3010")
+proxy=client.ServerProxy("http://localhost:3010") # replace localhost with server ip address when you are running server.py and client.py on different systems in LAN.
 num=int(input("Enter a number : "))
 print("Cube is {}".format(proxy.cube(num)))
